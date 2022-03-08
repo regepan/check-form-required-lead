364526025a280fcecd58f0e23e1114e32ef4bf6e
Sasaki Yuto <reggaepunch@gmail.com>
2022-03-08 14:01:56 +0700

    Update

--- check.js
+++ check.js
@@ -1,15 +1,20 @@
-if (-1 < location.search.indexOf("seminar_master_id")) {
-  var $link = $(".menu ul a:contains('キャンペーンテンプレート変更')");
+var $link = $(".section-register [onclick]");
 
-  // console.log($link.attr("href"));
-  window.location.href = $link.attr("href");
-}
+console.log($link);
 
-if (-1 < location.pathname.indexOf("seminartemplate/list")) {
-  $downloadLink = $(".hotListElement table.list tr.listRow:first a:contains('ダウンロード')");
 
-  // console.log($downloadLink);
+// if (-1 < location.search.indexOf("seminar_master_id")) {
+//   var $link = $(".menu ul a:contains('キャンペーンテンプレート変更')");
+//
+//   // console.log($link.attr("href"));
+//   // window.location.href = $link.attr("href");
+// }
 
-  window.location.href = $downloadLink.attr("href");
-  window.location.href = $downloadLink.attr("href");
-}
+// if (-1 < location.pathname.indexOf("seminartemplate/list")) {
+//   $downloadLink = $(".hotListElement table.list tr.listRow:first a:contains('ダウンロード')");
+//
+//   // console.log($downloadLink);
+//
+//   window.location.href = $downloadLink.attr("href");
+//   window.location.href = $downloadLink.attr("href");
+// }
